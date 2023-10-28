2e68fb470de845ac7c2d7a7c5efc47d55ed9e383
fathimathfida <fathimathfida01@gmail.com>
2023-10-28 21:44:45 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+void main()
+{
+
+  runApp(MaterialApp(home: Center(child: Text("welcome to flutter",style:TextStyle(fontFamily: 'yourcustomfont',fontSize:24.0,fontStyle: FontStyle.italic,color:Colors.pink,decoration:TextDecoration.none ) ,)),));
+
+}
